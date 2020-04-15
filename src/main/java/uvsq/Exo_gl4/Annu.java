5cea48e3c7f3fa029586bbe357add944e6252cae
uvsq21805839 <dskprod@gmail.com>
2020-04-15 08:25:51 +0200

    modification

--- src/main/java/uvsq/Exo_gl4/Annu.java
+++ src/main/java/uvsq/Exo_gl4/Annu.java
@@ -1,11 +1,12 @@
 package uvsq.Exo_gl4;
 
-import java.util.ArrayList;
-import java.util.List;
+
 
 public interface Annu
 {
      public void printAnnuaireName();
+
+	public void print();
 
 	
 }
---
+++ src/main/java/uvsq/Exo_gl4/PersonnelCompsitePattern.java
@@ -0,0 +1,51 @@
+package uvsq.Exo_gl4;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class PersonnelCompsitePattern
+implements Annu, Iterable<Annu> {
+	
+	private ArrayList<Annu> personnels;
+	
+	private final int id;
+	
+	private static int idNext = 1;
+	
+	public PersonnelCompsitePattern() {
+		id = idNext++;
+		personnels = new ArrayList<Annu>();
+	}
+	
+	public void print() {
+		System.out.println("Id : " + id);
+		for (Annu ip : personnels) {
+			ip.print();
+		}
+	}
+		
+	public PersonnelCompsitePattern add(final Annu ip) {
+		if (!personnels.contains(ip)) {
+			personnels.add(ip);
+		}
+		return this;
+	}
+	
+	public PersonnelCompsitePattern remove(final Annu ip) {
+		if (personnels.contains(ip)) {
+			personnels.remove(ip);
+		}
+		return this;
+	}
+	
+	public Iterator<Annu> iterator() {
+		return personnels.iterator();
+	}
+	
+	public final int getId() {
+		return id;
+	}
+	public void printAnnuaireName() {
+		// TODO Auto-generated method stub
+		
+	}
+}
