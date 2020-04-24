7284420868806cf25a360b25c271eaac0e335f37
uvsq21805839 <61480128+uvsq21805839@users.noreply.github.com>
2020-04-24 17:54:24 +0200

    Update personnel

--- Exo_gl4.1/src/main/java/uvsq/Exo_gl4/Personnel.java
+++ Exo_gl4.1/src/main/java/uvsq/Exo_gl4/Personnel.java
@@ -1,31 +1,42 @@
 package uvsq.Exo_gl4;
 
-import java.util.ArrayList;
-import java.time.LocalDate;
 
-public final class Personnel {
+public final class Personnel implements Annu{
+
+	 
 	private final String  nom;
+	public String getNom(){return nom;}
+	
 	private final String prenom;
+	 public String getPrenom() {
+	        return prenom;   }
+		
 	private final java.time.LocalDate dateNaissance;
-	private final ArrayList<Integer> numTelephone;
+	 public java.time.LocalDate getDateNaissance() {
+	        return dateNaissance;
+	    }
+	    
+	private final String numTelephone;
+//	@SuppressWarnings("unchecked")
+	public String getNumTelephone() {
+        return  numTelephone;
+    }
+	  
 
 	public static class Builder {
 		private final String  nom;
 		private final String prenom;
 		private final java.time.LocalDate dateNaissance;
-		private ArrayList<Integer> numTelephone;
+		private String numTelephone;
 		
-		public Builder(final String nom, final String prenom, final java.time.LocalDate dateNaissance) {
+		
+		public Builder(final String nom, final String prenom, final java.time.LocalDate dateNaissance,final String numTelephone) {
 			this.nom = nom;
-			this.prenom = prenom;
+			this.prenom = prenom; 
 			this.dateNaissance = dateNaissance;
-			
-			numTelephone = new ArrayList<Integer>();
-		}
-		
-		public Builder numeroTelephone(final  ArrayList<Integer> numTelephone) {
 			this.numTelephone = numTelephone;
-			return this;
+		
+	
 		}
 		
 		public Personnel build() {
@@ -38,5 +49,19 @@ public final class Personnel {
 		prenom = builder.prenom;
 		dateNaissance = builder.dateNaissance;
 		numTelephone = builder.numTelephone;
+		
+	 
 	}
+	public void print() {
+		
+		System.out.print(prenom + " " + nom	+" , naissance : " + dateNaissance + ", Numero de Telephone : " +  numTelephone );
+	
+		
+		
+	}
+	public void printAnnuaireName() {
+		// TODO Auto-generated method stub
+		
+	}
+	
 }
